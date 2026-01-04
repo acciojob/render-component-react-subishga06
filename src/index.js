7796c15bb9e32f1320699f26e90f0b56ee9ce039
subishga06 <subishgavs@gmail.com>
2026-01-04 19:13:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
 
+const App = () => {
+  return (
+    <p>I am learning React. My life is getting better.</p>
+  );
+};
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App;
